431f5f5d27c5d1a565bf6bad6df2256ed32608bc
Daniel <dnakhla@gmail.com>
2020-06-10 16:15:16 +0200

    load more on scroll
    Signed-off-by: Daniel <dnakhla@gmail.com>

--- frontend/src/components/CardsListView.vue
+++ frontend/src/components/CardsListView.vue
@@ -2,12 +2,33 @@
   <div>
     <h2>List</h2>
     <ul id="example-1">
-      <li v-for="card in list" :key="card.id">
-        {{ card.name }}
+      <li v-for="poke in list" :key="poke.id">
+        <div>
+          <img :src="poke.image" style="width:100px" />
+          {{ poke.name }}
+          <p>fav:{{ poke.isFavorite }}</p>
+          <p>types:{{ poke.types }}</p>
+        </div>
       </li>
     </ul>
   </div>
 </template>
+
+<style scoped lang="scss">
+ul {
+  list-style: none;
+  display: grid;
+  grid-template-columns: auto auto auto;
+  li {
+    display: grid;
+    width: 100%;
+    border: 1px solid saddlebrown;
+    div {
+      padding: 2rem;
+    }
+  }
+}
+</style>
 
 <script>
 export default {
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,27 +2,58 @@
   <div class="home">
     <Header />
     <FilterOptions :searchterm="searchTerm" />
-    <CardsListView :list="list" />
+    <CardsListView @scroll="handleScroll" :list="list" />
   </div>
 </template>
 
 <script>
 import { Header, FilterOptions, CardsListView } from "@/components/";
 import { getPokes } from "@/services/graphql-api/api";
+const NEW_CALL_COUNT = 18;
 export default {
   name: "app",
   data() {
     return {
       searchTerm: "",
-      list: [],
+      pageindex: 0,
+      list: null,
     };
   },
   async created() {
     try {
-      this.list = await getPokes(10, 0);
+      window.addEventListener("scroll", this.handleScroll);
+      this.list = await getPokes(NEW_CALL_COUNT, this.pageindex);
     } catch (e) {
-      console.error(e);
+      throw new Error(e.toString());
     }
+  },
+  methods: {
+    //@TOOD turn to mixin
+    handleScroll: async function() {
+      let bottomOfWindow =
+        Math.max(
+          window.pageYOffset,
+          document.documentElement.scrollTop,
+          document.body.scrollTop
+        ) +
+          window.innerHeight ===
+        document.documentElement.offsetHeight;
+
+      if (bottomOfWindow) {
+        try {
+          this.pageindex = this.pageindex + NEW_CALL_COUNT;
+          let newData = await getPokes(NEW_CALL_COUNT, this.pageindex);
+          if (newData.length > 0) {
+            let newState = new Set([...this.list, ...newData]);
+            this.list = [...newState];
+          } else {
+            window.removeEventListener("scroll", this.handleScroll);
+          }
+        } catch (e) {
+          console.error(e.toString());
+        }
+      }
+    },
   },
   components: {
     Header,
